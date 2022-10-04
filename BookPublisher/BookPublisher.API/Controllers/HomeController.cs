23559c94f34d64097d56de7a84db028b95d8a7cb
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-03 21:10:25 -0300

    Criando novo ControllerBase com teste do docker

---
+++ BookPublisher/BookPublisher.API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookPublisher.API.Controllers
+{
+    [ApiController]
+    [Route("Test")]
+    public class HomeController : ControllerBase
+    {
+        private static readonly string phrase = "Testando Docker!";
+
+        [HttpGet(Name = "Docker")]
+        public IActionResult Get()
+        {
+            return Ok(phrase);
+        }
+    }
+}
