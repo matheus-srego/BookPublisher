d99b20e5b4d7e6b41c97473abb6066f7ff6bd5f2
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-12 20:09:46 -0300

    Criando classe BookPublisherContext

---
+++ BookPublisher/BookPublisher.Persistence/Context/BookPublisherContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Persistence.Context
+{
+    public class BookPublisherContext : DbContext
+    {
+        public BookPublisherContext(DbContextOptions<BookPublisherContext> options) : base(options)
+        {
+            // ---
+        }
+    }
+}
