905ab9ea0bb121370c53d952f04085165d918b27
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-12 20:24:32 -0300

    Criando interface abstrata para o serviço

---
+++ BookPublisher/BookPublisher.Domain/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿using BookPublisher.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Domain.Interfaces.Services
+{
+    internal interface IBaseService<T> where T : BaseEntity
+    {
+        Task<T> Insert(T entity, CancellationToken cancellationToken = default);
+    }
+}
