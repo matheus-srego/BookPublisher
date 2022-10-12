9f90369e5543f1aaf0faa5ea2be041cd42cdab26
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-12 20:28:10 -0300

    Criando repositório abstrato

---
+++ BookPublisher/BookPublisher.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookPublisher.Domain.Interfaces.Repositories;
+using BookPublisher.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Persistence.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        public Task<T> Insert(T entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
