caf68b068255e3fcbb7017ddb29389f142b87de0
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-12 20:24:08 -0300

    Criando interface abstrata de repositórios

---
+++ BookPublisher/BookPublisher.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookPublisher.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<T> Insert(T entity, CancellationToken cancellationToken = default);
+    }
+}
