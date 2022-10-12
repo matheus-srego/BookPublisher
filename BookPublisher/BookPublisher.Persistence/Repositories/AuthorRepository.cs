a3258e3e51d13691602f5470aa9c493a32039a34
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-12 20:38:30 -0300

    Criando classe de implementação de repositório para autor

---
+++ BookPublisher/BookPublisher.Persistence/Repositories/AuthorRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookPublisher.Domain.Interfaces.Repositories;
+using BookPublisher.Domain.Models;
+using BookPublisher.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Persistence.Repositories
+{
+    internal sealed class AuthorRepository : BaseRepository<AuthorModel>, IAuthorRepository
+    {
+        public AuthorRepository(BookPublisherContext context) : base(context)
+        {
+            // ---
+        }
+    }
+}
