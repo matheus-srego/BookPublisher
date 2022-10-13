dea0d19386552dd9cb31618e5fdad76b8f00cd85
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-12 21:08:32 -0300

    Criando classe de serviço para autor
    Esta classe herda os métodos da classe abstrata de serviço

---
+++ BookPublisher/BookPublisher.Service/Services/AuthorService.cs
@@ -0,0 +1,19 @@
+﻿using BookPublisher.Domain.Interfaces.Repositories;
+using BookPublisher.Domain.Interfaces.Services;
+using BookPublisher.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Service.Services
+{
+    internal sealed class AuthorService : BaseService<AuthorModel>, IAuthorService
+    {
+        public AuthorService(IBaseRepository<AuthorModel> baseRepository) : base(baseRepository)
+        {
+            // ---
+        }
+    }
+}
