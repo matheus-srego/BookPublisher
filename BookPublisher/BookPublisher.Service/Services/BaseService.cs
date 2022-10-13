47a5e1f17dca299baf7ec4e6d6c270f753e3a5d7
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-12 21:08:02 -0300

    Criando classe abstrata de serviço

---
+++ BookPublisher/BookPublisher.Service/Services/BaseService.cs
@@ -0,0 +1,27 @@
+﻿using BookPublisher.Domain.Interfaces.Repositories;
+using BookPublisher.Domain.Interfaces.Services;
+using BookPublisher.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Service.Services
+{
+    internal class BaseService<T> : IBaseService<T> where T : BaseEntity
+    {
+        private readonly IBaseRepository<T> _baseRepository;
+
+        public BaseService(IBaseRepository<T> baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
+
+        public async Task<T> Insert(T entity, CancellationToken cancellationToken = default)
+        {
+            await _baseRepository.Insert(entity, cancellationToken);
+            return entity;
+        }
+    }
+}
