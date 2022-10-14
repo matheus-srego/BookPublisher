32beff7dcc910302c27c0e2cf0d2531d33f2590f
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-14 00:02:34 -0300

    Criando conexão com banco de dados

--- BookPublisher/BookPublisher.API/Program.cs
+++ BookPublisher/BookPublisher.API/Program.cs
@@ -1,6 +1,13 @@
+using BookPublisher.API;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+IConfiguration configuration = builder.Configuration;
+
+var startup = new Startup(configuration);
+startup.ConfigureServices(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ BookPublisher/BookPublisher.API/Startup.cs
@@ -0,0 +1,34 @@
+﻿using BookPublisher.Domain.Interfaces.Repositories;
+using BookPublisher.Domain.Interfaces.Services;
+using BookPublisher.Persistence.Context;
+using BookPublisher.Persistence.Repositories;
+using BookPublisher.Service.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookPublisher.API
+{
+    public class Startup
+    {
+        protected readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public virtual void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IAuthorService, AuthorService>();
+            serviceCollection.AddScoped<IAuthorRepository, AuthorRepository>();
+
+            var connection = _configuration["ConnectionString:editora-db"];
+
+            serviceCollection.AddDbContext<BookPublisherContext>(options => {
+
+                options.UseSqlServer(connection);
+
+            });
+
+            // serviceCollection.AddScoped<DbContext, BookPublisherContext>();
+        }
+    }
+}
