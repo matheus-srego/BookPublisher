b15ad3ac3642e9bffd226f6d136665e803b0b58b
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-16 15:41:39 -0300

    Criando controller base para autor

---
+++ BookPublisher/BookPublisher.API/Controllers/AuthorController.cs
@@ -0,0 +1,53 @@
+﻿using BookPublisher.Domain.DTOs;
+using BookPublisher.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace BookPublisher.API.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        // GET: api/<AuthorController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AuthorController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AuthorController>
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AuthorController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AuthorController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
