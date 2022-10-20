1ca6883f3938ab2d9b38a9f43295ca15a9df295c
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-19 23:17:47 -0300

    Criando modelo de livro

---
+++ BookPublisher/BookPublisher.Domain/Models/BookModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Domain.Models
+{
+    public class BookModel : BaseEntity
+    {
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
