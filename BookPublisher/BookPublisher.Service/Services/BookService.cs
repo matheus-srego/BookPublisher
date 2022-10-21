165653f4a12946d7803c10186a0e73f9f6418895
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-20 21:47:14 -0300

    Criando classe de serviço para livro

---
+++ BookPublisher/BookPublisher.Service/Services/BookService.cs
@@ -0,0 +1,19 @@
+﻿using BookPublisher.Domain.Interfaces.Repositories;
+using BookPublisher.Domain.Interfaces.Services;
+using BookPublisher.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Service.Services
+{
+    public sealed class BookService : BaseService<BookModel>, IBookService
+    {
+        public BookService(IBookRepository bookRepository) : base(bookRepository)
+        {
+            // ---
+        }
+    }
+}
