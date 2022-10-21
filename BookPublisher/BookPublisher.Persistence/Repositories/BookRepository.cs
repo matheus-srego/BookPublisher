9f349913394b248462e83c4c3485499681d6996b
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-20 21:45:05 -0300

    Criando classe de implementação de repositório para livro

---
+++ BookPublisher/BookPublisher.Persistence/Repositories/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookPublisher.Domain.Interfaces.Repositories;
+using BookPublisher.Domain.Models;
+using BookPublisher.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Persistence.Repositories
+{
+    public sealed class BookRepository : BaseRepository<BookModel>, IBookRepository
+    {
+        public BookRepository(BookPublisherContext context) : base(context)
+        {
+            // ---
+        }
+    }
+}
