1dfe6390dd0d1b5cb78415a32bbc5d03835e0025
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-20 23:14:12 -0300

    Criando método de update

--- BookPublisher/BookPublisher.API/Controllers/BookController.cs
+++ BookPublisher/BookPublisher.API/Controllers/BookController.cs
@@ -44,9 +44,13 @@ namespace BookPublisher.API.Controllers
         }
 
         // PUT api/<BookController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [ProducesResponseType(typeof(BookModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] BookModel bookModel)
         {
+            return Ok(await _bookService.UpdateAsync(bookModel));
         }
 
         // DELETE api/<BookController>/5
