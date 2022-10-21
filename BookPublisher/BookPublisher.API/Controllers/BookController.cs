12899edf64dd25627199efd1f0dc74862ed0eb9c
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-20 23:00:33 -0300

    Criando método de post

--- BookPublisher/BookPublisher.API/Controllers/BookController.cs
+++ BookPublisher/BookPublisher.API/Controllers/BookController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookPublisher.Domain.Interfaces.Services;
+using BookPublisher.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BookPublisher.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("/books")]
     [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
     public class BookController : ControllerBase
     {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
         // GET: api/<BookController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -23,9 +34,13 @@ namespace BookPublisher.API.Controllers
         }
 
         // POST api/<BookController>
+        [ProducesResponseType(typeof(BookModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> PostAsync([FromBody] BookModel bookModel)
         {
+            return Ok(await _bookService.InsertAsync(bookModel));
         }
 
         // PUT api/<BookController>/5
