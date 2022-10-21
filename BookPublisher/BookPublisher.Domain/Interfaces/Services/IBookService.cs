be406a7eff3168709ccfcdeb0e11f123be8e0e69
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-20 21:42:05 -0300

    Criando interface para o serviço de livro

---
+++ BookPublisher/BookPublisher.Domain/Interfaces/Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using BookPublisher.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Domain.Interfaces.Services
+{
+    public interface IBookService : IBaseService<BookModel>
+    {
+        // ---
+    }
+}
