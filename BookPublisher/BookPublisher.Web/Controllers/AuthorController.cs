5583dfe9c491df2ce1d8dcc7e7f1da550bf00d34
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-22 20:18:12 -0300

    Criando métodos de insert para autor

--- BookPublisher/BookPublisher.Web/Controllers/AuthorController.cs
+++ BookPublisher/BookPublisher.Web/Controllers/AuthorController.cs
@@ -13,3 +13,39 @@
 
             return View(list);
         }
+        public ViewResult Insert() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(AuthorModel model)
+        {
+            AuthorModel author = new AuthorModel();
+            using(var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+                using(var response = await httpClient.PostAsync(URL, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    author = JsonConvert.DeserializeObject<AuthorModel>(apiResponse);
+                }
+
+                return View(author);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            AuthorModel author = new AuthorModel();
+
+            using(var httpClient = new HttpClient())
+            {
+                using(var response = await httpClient.GetAsync(URL + "/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    author = JsonConvert.DeserializeObject<AuthorModel>(apiResponse);
+                }
+            }
+
+            return View(author);
+        }
