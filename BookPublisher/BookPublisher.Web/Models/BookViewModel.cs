89e596a778cdb397dc2d2e32dcd036bed27850e4
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-10-23 17:22:38 -0300

    Personalizando CRUD de livro

--- BookPublisher/BookPublisher.Web/Models/BookViewModel.cs
+++ BookPublisher/BookPublisher.Web/Models/BookViewModel.cs
@@ -1,9 +1,23 @@
-﻿namespace BookPublisher.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace BookPublisher.Web.Models
 {
     public class BookViewModel : AbstractViewModel
     {
+        [Required(ErrorMessage = "O campo 'Título' é obrigaqtório!")]
+        [StringLength(100)]
+        [Display(Name = "Título:")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "O campo 'ISBN' é obirgatório!")]
+        [StringLength(13)]
+        [Display(Name = "ISBN:")]
         public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Ano de Lançamento' é obrigatório!")]
+        [IntegerValidator]
+        [Display(Name = "Ano de Lançamento:")]
         public int ReleaseYear { get; set; }
     }
 }
