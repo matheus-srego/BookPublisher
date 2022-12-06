74163b45a28cfb8eb3b0c17e51f016b4a4f54952
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-12-06 20:01:53 -0300

    Creating DTO by insert user

--- BookPublisher/BookPublisher.Domain/DTOs/User/NewUserDTO.cs
+++ BookPublisher/BookPublisher.Domain/DTOs/User/NewUserDTO.cs
@@ -1,16 +1,22 @@
+using BookPublisher.Domain.Enums;
+
 namespace BookPublisher.Domain.DTOs.User
 {
     public class NewUserDTO
     {
         public string Name { get; set; }
+        public string Lastname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string UserType { get; set; }
 
-        public NewUserDTO(string name, string email, string password)
+        public NewUserDTO(string name, string lastname, string email, string password, string userType)
         {
             Name = name;
+            Lastname = lastname;
             Email = email;
-            Password = password;            
+            Password = password;
+            UserType = userType;
         }
     }
 }
