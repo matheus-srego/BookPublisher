41f428b53e6d0990476c06aaae83a07aecbe4044
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-12-06 19:57:54 -0300

    Creating new columns to user

--- sql/[table]usuario.sql
+++ sql/[table]usuario.sql
@@ -7,8 +7,10 @@ CREATE TABLE [dbo].[usuario]
 (
 	id INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
 	nome NVARCHAR(100) NOT NULL,
+	sobrenome NVARCHAR(100) NOT NULL,
 	email NVARCHAR(50) UNIQUE NOT NULL,
-	senha NVARCHAR(256) NOT NULL
+	senha NVARCHAR(256) NOT NULL,
+	tipo_usuario INT NOT NULL CHECK(tipo_usuario IN(0, 1))
 );
 GO
 
