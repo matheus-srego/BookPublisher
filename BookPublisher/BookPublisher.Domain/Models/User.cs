cd675555605d80bc3fea2d6d03c927974cd3bd93
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-12-06 20:02:28 -0300

    Adding new attributes to user

--- BookPublisher/BookPublisher.Domain/Models/User.cs
+++ BookPublisher/BookPublisher.Domain/Models/User.cs
@@ -1,16 +1,22 @@
-﻿namespace BookPublisher.Domain.Models
+﻿using BookPublisher.Domain.Enums;
+
+namespace BookPublisher.Domain.Models
 {
     public class User : BaseEntity
     {
         public string Name { get; set; }
+        public string Lastname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public UserType UserType { get; set; }
 
-        public User(string name, string email, string password)
+        public User(string name, string lastname, string email, string password, UserType userType)
         {
             Name = name;
+            Lastname = lastname;
             Email = email;
-            Password = password;            
+            Password = password;
+            UserType = userType;
         }
     }
 }
